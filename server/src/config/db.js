9ecb09d176433eae38cbf96d308b2119c94b3cc9
wilsalasgz <wilsalasgz@gmail.com>
2019-03-07 22:16:56 -0500

    make it better

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 //configuration for accessing the mongodb database
 (async () => {
     try {
-        await mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
+        await mongoose.connect(process.env.DB_HOST, {
+            auth: {
+                user: process.env.DB_USER,
+                password: process.env.DB_PASS,
+            }
+            , useNewUrlParser: true
+        });
         console.log("Connected to MongoDB");
     } catch (err) {
         console.log(`An error occurred connecting to MongoDB ${err}`);
